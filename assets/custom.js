5f967680868a4678e13d440e1c40c131bb7fe6b8
Bailey Latimer <latimerdesignco@gmail.com>
2022-10-25 20:56:34 -0700

    added testimonial slider

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
+  console.log('✅ testimonial')
   $('.testimonial-slider').slick({
-    setting-name: setting-value
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    fade: true,
+    centerMode: true,
+    slickPlay: true
   });
 });
